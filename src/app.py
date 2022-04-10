3aa5c31390bfe849c30933ffe9d112abd7821a9b
Mima2370 <37240450+Mima2370@users.noreply.github.com>
2022-04-10 19:20:33 +0200

    Update

--- src/app.py
+++ src/app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 app.register_blueprint(routes, url_prefix='/star')
 
 if __name__ == '__main__':
-    app.run(port=80, host='gl-server.herokuapp.com', debug=True)
+    app.run(port=80, debug=True)
