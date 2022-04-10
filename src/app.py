8b17ae410ff346f955c7aa70af04621fe9b4b14d
Mima2370 <37240450+Mima2370@users.noreply.github.com>
2022-04-10 19:06:07 +0200

    Update

--- src/app.py
+++ src/app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 app.register_blueprint(routes, url_prefix='/star')
 
 if __name__ == '__main__':
-    app.run(port=80, host='192.168.195.245', debug=True)
+    app.run(port=80, host='0.0.0.0', debug=True)
