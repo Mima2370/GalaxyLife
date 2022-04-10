420409aa46b82ca6c685a15f87363321328e0348
Mima2370 <37240450+Mima2370@users.noreply.github.com>
2022-04-10 20:28:46 +0200

    Update

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 from api.api import routes
@@ -7,4 +8,6 @@ app = Flask(__name__)
 app.register_blueprint(routes, url_prefix='/star')
 
 if __name__ == '__main__':
-    app.run(port=80, host='0.0.0.0', debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    print(port)
+    app.run(port=port, host='0.0.0.0', debug=True)
